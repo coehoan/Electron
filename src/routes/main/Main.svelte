2a5c34f2dd79509d6ac865a5c412c28fc1de1c18
coehoan <coehoan@naver.com>
2023-10-18 17:33:14 +0900

    - main 화면 변경

--- src/routes/main/Main.svelte
+++ src/routes/main/Main.svelte
@@ -24,14 +24,48 @@
     })
 </script>
 
-<main>
+<main style="height: 100%">
     <Header {title}/>
-    <p on:click={() => {push('/info')}}>기관정보</p>
-    <p on:click={() => {push('/self')}}>자체평가</p>
-    <p on:click={() => {push('/inspect')}}>현장실사</p>
-    <p on:click={() => {push('/result')}}>평가관리</p>
+    <div style="margin-top: 10px; height: 90%; display: flex; align-items: center; justify-content: center; flex-direction: column">
+        <div class="main-wrap">
+            <div class="main-button" on:click={() => {push('/info')}}>
+                <p>기관정보</p>
+            </div>
+            <div class="main-button" on:click={() => {push('/self')}}>
+                <p>자체평가</p>
+            </div>
+        </div>
+        <div class="main-wrap" style="margin-top: 10px">
+            <div class="main-button" on:click={() => {push('/inspect')}}>
+                <p>현장실사</p>
+            </div>
+            <div class="main-button" on:click={() => {push('/result')}}>
+                <p>평가관리</p>
+            </div>
+        </div>
+    </div>
 </main>
 
 <style>
-
+    .main-wrap {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .main-button {
+        width: 49.5%;
+        height: 100%;
+        border: 1px solid gray;
+        border-radius: 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor:pointer;
+        box-shadow: 1px 1px 1px gray;
+    }
+    .main-button:hover {
+        background-color: #999999;
+    }
 </style>
